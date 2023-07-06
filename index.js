014d7526304e00bc4efedcc412810548a3470b15
Lucas Santiago <izaias.santiago.07@gmail.com>
2023-07-06 13:09:01 -0300

    Transformar código solto em função
    Após criar a lógica, a ideia foi realizar o clean code e deixar o código reutilizável

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-var number = 34
-var binaru = ''
-
-for (let i = 2; number>=i;) {
-    
-    let rest = number%i
-    number = parseInt(number/i)
-    binaru += rest
-    if (number<i) {
-        binaru += number
+var binary = (num) => {
+    let number = num
+    let binary = ''
+    for (let i = 2;number>=i;) {
+        let rest = number%i
+        number = parseInt(number/i)
+        binary += rest
+        if (number<i) {
+            binary += number
+        }
     }
+
+    let binaryum = ''
+
+for (let i = binary.length-1;i>=0;i--) {
+    binaryum += binary[i]
     
 }
-var binary = ''
-
-for (let i = binaru.length-1;i>=0;i--) {
-    binary += binaru[i]
+binary = null
     
-}
-binaru = null
-console.log (binary)
-var x = 0
-var Decimal = 0
-for (let i = binary.length-1;i>=0;i--){
-    Decimal += parseInt(binary[i])*Math.pow(2,x)
-    x++
+    return binaryum
 }
 
-console.log (Decimal)
+console.log(binary(12))
+
+
+function converterBinaryToDecimal (num) {
+    let x = 0
+    let decimal = 0
+    for (let i = num.length-1;i>=0;i--){
+        decimal += parseInt(num[i])*Math.pow(2,x)
+        x++
+    }
+    return decimal
+}
+    console.log (converterBinaryToDecimal(binary(5)))
